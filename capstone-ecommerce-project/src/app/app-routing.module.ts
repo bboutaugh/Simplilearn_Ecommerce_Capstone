7601bc5a7bfdff75fa90024ae7a870a9aa9b8136
bboutaugh <brad.boutaugh@gmail.com>
2020-11-10 16:44:21 -0600

    setup home component general design

--- capstone-ecommerce-project/src/app/app-routing.module.ts
+++ capstone-ecommerce-project/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
-const routes: Routes = [];
+import {AppComponent} from './app.component';
+import {HomeComponent} from './home/home.component';
+export const routes: Routes = [
+  {path:'',redirectTo:'home', pathMatch:'full'},
+  {path:'home',component:HomeComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- capstone-ecommerce-project/src/app/app.module.ts
+++ capstone-ecommerce-project/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {FormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -26,7 +27,9 @@ import { AboutComponent } from './about/about.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
